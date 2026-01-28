bf89440aa3c7265e9b7c5a233501d638d457c1ba
Srikanth <srikanthjava91@gmail.com>
2026-01-28 11:27:28 +0530

    28th Coomit

---
+++ batch69firstproject/src/com/logicalstatements/loops/TestLPDemo1.java
@@ -0,0 +1,29 @@
+package com.logicalstatements.loops;
+
+//Q) WAP to print 10 values like from 0 to 10, every value which increase every time 1.
+//Q) WAP to print 10 values like from 0 to 10, every value which increase every time 2.
+//Q) WAP to print 10 values like from 10 to 0, every value which decrease every time 1.
+//Q) WAP to print 10 values like from 10 to 0, every value which decrease every time 2. and print odd numbers.
+public class TestLPDemo1 {
+
+	public static void main(String[] args) {
+
+//		for (int i = 0; i <= 10; i++) {
+//			System.out.println("Hello-" + i);//1 2 3 4 5 
+//		}
+
+//		for (int i = 0; i <= 10; i = i + 2) {
+//			System.out.println("Hi : " + i );
+//		}
+
+//		for (int i = 9; i >= 0;i--) {
+//			System.out.print(i + " ");
+//		}
+
+//		for (int i = 9; i >= 0; i = i - 2) {
+//			System.out.print(i + " ");
+//		}
+
+	}
+
+}
